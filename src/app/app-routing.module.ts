f399d68c8fc268e6171a9d5e1216182c37e34e30
IGSparkew <40170910+IGSparkew@users.noreply.github.com>
2023-07-07 19:52:53 +0200

    Ajout architecture mais ne fonctionne pas encore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SmartComponent } from './hexa-sport-resa/vue/smart/smart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path:'',
+        component:SmartComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: '/login',
+                pathMatch: 'full'
+            },
+            {
+                path: 'login',
+                loadChildren: () => import('./hexa-sport-resa/vue/dumb/login/login.module').then(m => m.LoginModule),
+            }
+        ]
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
---
+++ src/app/hexa-sport-resa/hexa-sport-resa.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './vue/dumb/login/login.component';
+import { RegisterComponent } from './vue/dumb/register/register.component';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { LoginModule } from './vue/dumb/login/login.module';
+
+
+@NgModule({
+    declarations: [
+        
+    ],
+    imports: [
+        CommonModule,
+        BrowserModule,
+        LoginModule
+    ], 
+    exports: [
+        LoginModule
+    ]
+})
+export class HexaSportResaModule {}
